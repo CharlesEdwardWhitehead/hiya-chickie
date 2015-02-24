c33de42986f7b794433d1dd3fd443da63893e527
Charles Edward Whitehead <softmachineman71@gmail.com>
2015-02-24 11:35:00 -0600

    Modified encoding for the latest version of Python, using parentheses for all print commands, and using input() in place of raw_input().

---
+++ Python code sample 01.py
@@ -0,0 +1,9 @@
+print ("How old are you?",)
+age = input()
+print ("How tall are you?",)
+height = input()
+print ("How much do you weigh?",)
+weight = input()
+
+print ("So, you're %r old, %r tall and %r pounds." % (
+    age, height, weight))
